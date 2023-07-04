5f16dd617f1222645dc684b13b7388fb60c56ba0
v_zpgong <v_zpgong@tencent.com>
2023-07-04 11:15:53 +0800

    feat: 添加页面过渡动画

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -44,7 +44,17 @@
       </el-header>
       <!-- 右侧内容主体 -->
       <el-main>
-        <router-view></router-view>
+        <keep-alive>
+          <router-view v-if="$route.meta.keepAlive">
+            <!-- 这里是会被缓存的视图组件-->
+          </router-view>
+        </keep-alive>
+
+        <transition name="fade-transform" mode="out-in">
+          <router-view v-if="!$route.meta.keepAlive">
+            <!-- 这里是不被缓存的视图组件 -->
+          </router-view>
+        </transition>
       </el-main>
     </el-container>
   </el-container>
